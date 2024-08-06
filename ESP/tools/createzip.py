1f6b52a39ba4b0b94601f45f600b4c1be0fc62ac
Pho Hale <PhoHale@gmail.com>
2024-08-06 19:27:06 -0400

    bump

--- ESP/tools/createzip.py
+++ ESP/tools/createzip.py
@@ -157,99 +157,9 @@ def createZip(source, target, env):
         if not os.path.exists(archive_folder):
             os.makedirs(archive_folder)
         
-        # with ZipFile(zip_file_path, "w") as archive:
-        #     print('\nCreating "' + archive.filename + '"', end="\n")
-        #     parts = []
-
-        #     name = "OpenIris"
-        #     version = env["PROGNAME"].split("-")[1]
-        #     new_install_prompt_erase = True
-
-        #     print("Creating manifest.json")
-
-        #     temp = []
-        #     for [offset, path] in partitions:
-        #         # join the offset and path into a space separated string
-        #         temp.append("{} {}".format(offset, path))
-
-        #     # detect if the PIOENV has QIO flash mode
-        #     flash_mode = env["BOARD_FLASH_MODE"]
-
-        #     flash_freq = env["BOARD_F_FLASH"]
-
-        #     if flash_freq == "80000000L":
-        #         flash_freq = "80m"
-        #     elif flash_freq == "40000000L":
-        #         flash_freq = "40m"
-
-        #     # detect the chip type
-        #     chip_type = env["BOARD_MCU"]
-
-        #     if chip_type == "esp32":
-        #         flash_size = '4'
-        #     elif chip_type == "esp32s2":
-        #         flash_size = '2'
-        #     elif chip_type == "esp32c3":
-        #         flash_size = '2'
-        #     elif chip_type == "esp32s3":
-        #         flash_size = '8'
-
-
-        #     if chip_type == "esp32c3" or chip_type == "esp32s3" or chip_type == "esp32s2":
-        #         chip_type = chip_type.replace("esp32", "esp32-")
-
-        #     # capitalize the chip type
-        #     chip_type = chip_type.upper()
-
-        #     sys.stdout.write(BLUE)
-        #     print("Flash Mode: %s" % flash_mode)
-        #     print("Chip Type: %s" % chip_type)
-        #     print("Flash Frequency: %s" % flash_freq)
-        #     print("Flash Size: %s" % flash_size)
-
-        #     """
-        #     python esptool.py --chip ESP32 merge_bin -o merged-firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
-        #     0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot.bin 0x10000 OpenIris-v1.3.0-esp32AIThinker-8229a3a-master.bin
-        #     """
-        #     execute_cmd = "$PYTHONEXE $PROJECT_PACKAGES_DIR/tool-esptoolpy/esptool.py --chip {chip} merge_bin -o merged-firmware.bin --flash_mode {flash_mode} --flash_freq {flash_freq} --flash_size {flash_size}MB %s" % (
-        #         " ".join(temp)
-        #     )
-
-        #     print("Executing: %s" % execute_cmd)
-
-        #     sys.stdout.write(RESET)
-
-        #     env.Execute(
-        #         execute_cmd.format(
-        #             chip=chip_type, flash_mode=flash_mode, flash_freq=flash_freq, flash_size=flash_size)
-        #     )
-
-        #     filename = basename("merged-firmware.bin")
-        #     # filename = f"{}/merged-firmware.bin"
-        #     archive.write("merged-firmware.bin", filename)
-
-        #     partition = {
-        #         "path": filename,
-        #         "offset": 0,
-        #     }
-        #     parts.append(partition)
-
-        #     manifest = {
-        #         "name": name,
-        #         "version": version,
-        #         "new_install_prompt_erase": new_install_prompt_erase,
-        #         "builds": [
-        #             {
-        #                 "chipFamily": chip_type,
-        #                 "parts": parts,
-        #             }
-        #         ],
-        #     }
-        #     archive.writestr("manifest.json", json.dumps(manifest))
         archive_outputs = _build_manifest_json(env, partitions)
         print(f'archive_outputs: {archive_outputs}')
         
-        archive_folder
 
         _out_merged_firmware_path = Path(archive_outputs["merged-firmware.bin"]).resolve()
         assert _out_merged_firmware_path.exists()
@@ -265,7 +175,14 @@ def createZip(source, target, env):
         manifest_txt_path.write_text(archive_outputs["manifest.json"]) # write to file 
         # archive_outputs["manifest.json"]
 
-
+        # Create a zip archive of the target directory
+        # zip_name = os.path.join(build_dir, f"{target}.zip")
+        
+        # make_archive(zip_file_path.replace('.zip', ''), 'zip', archive_folder)
+        final_relative_zip_basename_path = _out_zip_parent_path.joinpath(zip_file_path.stem).resolve()
+        make_archive(final_relative_zip_basename_path, format='zip', root_dir=archive_folder)
+        print(f"Created zip archive '{zip_file_path}'")
+        
         sys.stdout.write(RESET)
 
     else:
