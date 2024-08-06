9764e64e1edaed8a99a6d24a1ca62714be572a96
Pho Hale <PhoHale@gmail.com>
2024-08-06 19:08:54 -0400

    getting image output working

--- ESP/tools/createzip.py
+++ ESP/tools/createzip.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 Import("env")
 
+import shutil
 import sys
 from colors import *
 import os
@@ -9,6 +10,100 @@ from ntpath import basename
 from zipfile import ZipFile
 import json
 from shutil import copyfile, make_archive
+
+def _build_manifest_json(env, partitions):
+    archive_outputs = {}
+    
+    parts = []
+
+    name = "OpenIris"
+    version = env["PROGNAME"].split("-")[1]
+    new_install_prompt_erase = True
+
+    print("Creating manifest.json")
+
+    temp = []
+    for [offset, path] in partitions:
+        # join the offset and path into a space separated string
+        temp.append("{} {}".format(offset, path))
+
+    # detect if the PIOENV has QIO flash mode
+    flash_mode = env["BOARD_FLASH_MODE"]
+
+    flash_freq = env["BOARD_F_FLASH"]
+
+    if flash_freq == "80000000L":
+        flash_freq = "80m"
+    elif flash_freq == "40000000L":
+        flash_freq = "40m"
+
+    # detect the chip type
+    chip_type = env["BOARD_MCU"]
+
+    if chip_type == "esp32":
+        flash_size = '4'
+    elif chip_type == "esp32s2":
+        flash_size = '2'
+    elif chip_type == "esp32c3":
+        flash_size = '2'
+    elif chip_type == "esp32s3":
+        flash_size = '8'
+
+
+    if chip_type == "esp32c3" or chip_type == "esp32s3" or chip_type == "esp32s2":
+        chip_type = chip_type.replace("esp32", "esp32-")
+
+    # capitalize the chip type
+    chip_type = chip_type.upper()
+
+    sys.stdout.write(BLUE)
+    print("Flash Mode: %s" % flash_mode)
+    print("Chip Type: %s" % chip_type)
+    print("Flash Frequency: %s" % flash_freq)
+    print("Flash Size: %s" % flash_size)
+
+    """
+    python esptool.py --chip ESP32 merge_bin -o merged-firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
+    0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot.bin 0x10000 OpenIris-v1.3.0-esp32AIThinker-8229a3a-master.bin
+    """
+    execute_cmd = "$PYTHONEXE $PROJECT_PACKAGES_DIR/tool-esptoolpy/esptool.py --chip {chip} merge_bin -o merged-firmware.bin --flash_mode {flash_mode} --flash_freq {flash_freq} --flash_size {flash_size}MB %s" % (
+        " ".join(temp)
+    )
+
+    print("Executing: %s" % execute_cmd)
+
+    sys.stdout.write(RESET)
+
+    env.Execute(
+        execute_cmd.format(
+            chip=chip_type, flash_mode=flash_mode, flash_freq=flash_freq, flash_size=flash_size)
+    )
+
+    filename = basename("merged-firmware.bin")
+    # archive.write("merged-firmware.bin", filename)
+    archive_outputs["merged-firmware.bin"] = filename
+    
+    partition = {
+        "path": filename,
+        "offset": 0,
+    }
+    parts.append(partition)
+
+    manifest = {
+        "name": name,
+        "version": version,
+        "new_install_prompt_erase": new_install_prompt_erase,
+        "builds": [
+            {
+                "chipFamily": chip_type,
+                "parts": parts,
+            }
+        ],
+    }
+    # archive.writestr("manifest.json", json.dumps(manifest))
+    archive_outputs["manifest.json"] = json.dumps(manifest)
+    sys.stdout.write(RESET)
+    return archive_outputs
 
 def createZip(source, target, env):
     if os.getenv("OPENIRIS_CI_BUILD", False):
@@ -39,104 +134,139 @@ def createZip(source, target, env):
             for i in range((len(partitions_arg) + n - 1) // n)
         ]
         print(f"partitions: {partitions}")
-        file_name = "./build/{0}/{1}.zip".format(
+        # file_name = "./build/{0}/{1}.zip".format(
+        file_name = "{0}/{1}.zip".format(
             str(env["PIOENV"]), env["PROGNAME"]
         )
         _out_zip_parent_path = Path(r"C:\Users\pho\repos\ExternalTesting\OpenIris\ESP\build")
         assert _out_zip_parent_path.exists()
-        print(f"file_name: '{file_name}'")
+        print(f"_out_zip_parent_path: '{_out_zip_parent_path}'\nfile_name: '{file_name}'")
         zip_file_path = _out_zip_parent_path.joinpath(file_name).resolve()
-        print(f"zip_file_path: '{zip_file_path}'")
+        print(f"zip_file_path: '{zip_file_path.as_posix()}'")
+        # ensure that the parent directory exists!
+        _immediate_parent_directory = zip_file_path.parent.resolve()
+        if _immediate_parent_directory.is_dir():
+            print(f'creating parent dir: "{_immediate_parent_directory.as_posix()}" because it did not exist.')
+            _immediate_parent_directory.mkdir(parents=True, exist_ok=True) # create the parent directory if needed
+
+        print(f'building manifest...\n')
+        # print("Creating manifest.json")
+        archive_folder = zip_file_path.with_suffix('') # no suffix
         
-        with ZipFile(zip_file_path, "w") as archive:
-            print('\nCreating "' + archive.filename + '"', end="\n")
-            parts = []
+        print('\nCreating archive_folder "' + archive_folder.as_posix() + '"', end="\n")
+        if not os.path.exists(archive_folder):
+            os.makedirs(archive_folder)
+        
+        # with ZipFile(zip_file_path, "w") as archive:
+        #     print('\nCreating "' + archive.filename + '"', end="\n")
+        #     parts = []
 
-            name = "OpenIris"
-            version = env["PROGNAME"].split("-")[1]
-            new_install_prompt_erase = True
+        #     name = "OpenIris"
+        #     version = env["PROGNAME"].split("-")[1]
+        #     new_install_prompt_erase = True
 
-            print("Creating manifest.json")
+        #     print("Creating manifest.json")
 
-            temp = []
-            for [offset, path] in partitions:
-                # join the offset and path into a space separated string
-                temp.append("{} {}".format(offset, path))
+        #     temp = []
+        #     for [offset, path] in partitions:
+        #         # join the offset and path into a space separated string
+        #         temp.append("{} {}".format(offset, path))
 
-            # detect if the PIOENV has QIO flash mode
-            flash_mode = env["BOARD_FLASH_MODE"]
+        #     # detect if the PIOENV has QIO flash mode
+        #     flash_mode = env["BOARD_FLASH_MODE"]
 
-            flash_freq = env["BOARD_F_FLASH"]
+        #     flash_freq = env["BOARD_F_FLASH"]
 
-            if flash_freq == "80000000L":
-                flash_freq = "80m"
-            elif flash_freq == "40000000L":
-                flash_freq = "40m"
+        #     if flash_freq == "80000000L":
+        #         flash_freq = "80m"
+        #     elif flash_freq == "40000000L":
+        #         flash_freq = "40m"
 
-            # detect the chip type
-            chip_type = env["BOARD_MCU"]
+        #     # detect the chip type
+        #     chip_type = env["BOARD_MCU"]
 
-            if chip_type == "esp32":
-                flash_size = '4'
-            elif chip_type == "esp32s2":
-                flash_size = '2'
-            elif chip_type == "esp32c3":
-                flash_size = '2'
-            elif chip_type == "esp32s3":
-                flash_size = '8'
+        #     if chip_type == "esp32":
+        #         flash_size = '4'
+        #     elif chip_type == "esp32s2":
+        #         flash_size = '2'
+        #     elif chip_type == "esp32c3":
+        #         flash_size = '2'
+        #     elif chip_type == "esp32s3":
+        #         flash_size = '8'
 
 
-            if chip_type == "esp32c3" or chip_type == "esp32s3" or chip_type == "esp32s2":
-                chip_type = chip_type.replace("esp32", "esp32-")
+        #     if chip_type == "esp32c3" or chip_type == "esp32s3" or chip_type == "esp32s2":
+        #         chip_type = chip_type.replace("esp32", "esp32-")
 
-            # capitalize the chip type
-            chip_type = chip_type.upper()
+        #     # capitalize the chip type
+        #     chip_type = chip_type.upper()
 
-            sys.stdout.write(BLUE)
-            print("Flash Mode: %s" % flash_mode)
-            print("Chip Type: %s" % chip_type)
-            print("Flash Frequency: %s" % flash_freq)
-            print("Flash Size: %s" % flash_size)
+        #     sys.stdout.write(BLUE)
+        #     print("Flash Mode: %s" % flash_mode)
+        #     print("Chip Type: %s" % chip_type)
+        #     print("Flash Frequency: %s" % flash_freq)
+        #     print("Flash Size: %s" % flash_size)
 
-            """
-            python esptool.py --chip ESP32 merge_bin -o merged-firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
-            0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot.bin 0x10000 OpenIris-v1.3.0-esp32AIThinker-8229a3a-master.bin
-            """
-            execute_cmd = "$PYTHONEXE $PROJECT_PACKAGES_DIR/tool-esptoolpy/esptool.py --chip {chip} merge_bin -o merged-firmware.bin --flash_mode {flash_mode} --flash_freq {flash_freq} --flash_size {flash_size}MB %s" % (
-                " ".join(temp)
-            )
+        #     """
+        #     python esptool.py --chip ESP32 merge_bin -o merged-firmware.bin --flash_mode dio --flash_freq 40m --flash_size 4MB
+        #     0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot.bin 0x10000 OpenIris-v1.3.0-esp32AIThinker-8229a3a-master.bin
+        #     """
+        #     execute_cmd = "$PYTHONEXE $PROJECT_PACKAGES_DIR/tool-esptoolpy/esptool.py --chip {chip} merge_bin -o merged-firmware.bin --flash_mode {flash_mode} --flash_freq {flash_freq} --flash_size {flash_size}MB %s" % (
+        #         " ".join(temp)
+        #     )
 
-            print("Executing: %s" % execute_cmd)
+        #     print("Executing: %s" % execute_cmd)
 
-            sys.stdout.write(RESET)
+        #     sys.stdout.write(RESET)
 
-            env.Execute(
-                execute_cmd.format(
-                    chip=chip_type, flash_mode=flash_mode, flash_freq=flash_freq, flash_size=flash_size)
-            )
+        #     env.Execute(
+        #         execute_cmd.format(
+        #             chip=chip_type, flash_mode=flash_mode, flash_freq=flash_freq, flash_size=flash_size)
+        #     )
 
-            filename = basename("merged-firmware.bin")
-            archive.write("merged-firmware.bin", filename)
+        #     filename = basename("merged-firmware.bin")
+        #     # filename = f"{}/merged-firmware.bin"
+        #     archive.write("merged-firmware.bin", filename)
 
-            partition = {
-                "path": filename,
-                "offset": 0,
-            }
-            parts.append(partition)
+        #     partition = {
+        #         "path": filename,
+        #         "offset": 0,
+        #     }
+        #     parts.append(partition)
 
-            manifest = {
-                "name": name,
-                "version": version,
-                "new_install_prompt_erase": new_install_prompt_erase,
-                "builds": [
-                    {
-                        "chipFamily": chip_type,
-                        "parts": parts,
-                    }
-                ],
-            }
-            archive.writestr("manifest.json", json.dumps(manifest))
-            sys.stdout.write(RESET)
+        #     manifest = {
+        #         "name": name,
+        #         "version": version,
+        #         "new_install_prompt_erase": new_install_prompt_erase,
+        #         "builds": [
+        #             {
+        #                 "chipFamily": chip_type,
+        #                 "parts": parts,
+        #             }
+        #         ],
+        #     }
+        #     archive.writestr("manifest.json", json.dumps(manifest))
+        archive_outputs = _build_manifest_json(env, partitions)
+        print(f'archive_outputs: {archive_outputs}')
+        
+        archive_folder
+
+        _out_merged_firmware_path = Path(archive_outputs["merged-firmware.bin"]).resolve()
+        assert _out_merged_firmware_path.exists()
+        # copy it
+        archive_folder = Path(archive_folder).resolve()
+        archive_folder_out_merged_firmware_path = archive_folder.joinpath(_out_merged_firmware_path.name).resolve()
+        shutil.copy(_out_merged_firmware_path, archive_folder_out_merged_firmware_path)
+        print(f"Copied '{_out_merged_firmware_path.as_posix()}' to '{archive_folder_out_merged_firmware_path.as_posix()}'")        
+
+        
+        manifest_txt_path = archive_folder.joinpath("manifest.json").resolve()
+        print(f'writing out manifest.json to "{manifest_txt_path.as_posix()}"...')
+        manifest_txt_path.write_text(archive_outputs["manifest.json"]) # write to file 
+        # archive_outputs["manifest.json"]
+
+
+        sys.stdout.write(RESET)
 
     else:
         sys.stdout.write(BLUE)
--- repo-tools/scripts/build_all_local.py
+++ repo-tools/scripts/build_all_local.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 import sys
-from shutil import copyfile
+from shutil import copyfile, make_archive
+from pathlib import Path
 
 # Define the target environments
 # targets = ["esp32AIThinker", "esp32M5Stack", "esp32Cam", "esp_eye", "wrover", "wrooms3QIO", "wrooms3QIOUSB", "wrooms3", "wrooms3USB", "xiaosenses3", "xiaosenses3_USB"]
@@ -11,13 +12,16 @@ targets = ["xiaosenses3_phoeyeleft", "xiaosenses3_phoeyeright", "xiaosenses3_pho
 # Set the CI build environment variable
 os.environ["OPENIRIS_CI_BUILD"] = "1"
 
+esp_dir = Path(r'C:\Users\pho\repos\ExternalTesting\OpenIris\ESP').resolve() #os.getcwd()
+assert esp_dir.exists()
+
 # Create the build directory if it doesn't exist
-build_dir = os.path.join(os.getcwd(), "build")
+build_dir = os.path.join(esp_dir, "build")
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
 
 def run_command(command):
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=esp_dir.as_posix())
     if result.returncode != 0:
         print(f"Command failed with error: {result.stderr}")
         sys.exit(1)
@@ -29,19 +33,47 @@ for target in targets:
     run_command(f"pio run --environment {target}")
     print(f"Build for {target} completed.")
 
-    # Create zip file for the build
+    # Create target directory if it doesn't exist
     target_dir = os.path.join(build_dir, target)
-    zip_files = [f for f in os.listdir(target_dir) if f.endswith(".zip")]
-    if not zip_files:
-        print(f"No zip files found in {target_dir}")
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    # Locate the firmware binary
+    firmware_bin = os.path.join(esp_dir, ".pio", "build", target, "firmware.bin")
+    if not os.path.exists(firmware_bin):
+        print(f"No firmware binary found for {target}")
         continue
 
-    for zip_file in zip_files:
-        zip_path = os.path.join(target_dir, zip_file)
-        run_command(f"unzip -l {zip_path}")
+    # Copy firmware binary to target directory
+    firmware_bin_dst = os.path.join(target_dir, "firmware.bin")
+    copyfile(firmware_bin, firmware_bin_dst)
+    print(f"Copied firmware binary to {firmware_bin_dst}")
 
     # Copy firmware_name.txt to the target directory
-    firmware_name_src = os.path.join(os.getcwd(), "tools", "firmware_name.txt")
+    firmware_name_src = os.path.join(esp_dir, "tools", "firmware_name.txt")
     firmware_name_dst = os.path.join(target_dir, "firmware_name.txt")
     copyfile(firmware_name_src, firmware_name_dst)
     print(f"Copied firmware_name.txt to {firmware_name_dst}")
+
+    # Create a zip archive of the target directory
+    zip_name = os.path.join(build_dir, f"{target}.zip")
+    make_archive(zip_name.replace('.zip', ''), 'zip', target_dir)
+    print(f"Created zip archive {zip_name}.zip")
+    
+
+    # # Create zip file for the build
+    # target_dir = os.path.join(build_dir, target)
+    # zip_files = [f for f in os.listdir(target_dir) if f.endswith(".zip")]
+    # if not zip_files:
+    #     print(f"No zip files found in {target_dir}")
+    #     continue
+
+    # for zip_file in zip_files:
+    #     zip_path = os.path.join(target_dir, zip_file)
+    #     run_command(f"unzip -l {zip_path}")
+
+    # # Copy firmware_name.txt to the target directory
+    # firmware_name_src = os.path.join(esp_dir, "tools", "firmware_name.txt")
+    # firmware_name_dst = os.path.join(target_dir, "firmware_name.txt")
+    # copyfile(firmware_name_src, firmware_name_dst)
+    # print(f"Copied firmware_name.txt to {firmware_name_dst}")
